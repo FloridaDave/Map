14e2f91dc496625bfb1b8ee9c1e22bc82a75e0c8
FloridaDave <tohighdesertdave@yahoo.com>
2016-08-25 12:05:44 -0400

    added missing ;

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
 
-    $greeting.text( 'So, you want to live at ' + address +'?')
+    $greeting.text( 'So, you want to live at ' + address +'?');
 
     varStreetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address +'';
     $body.append('img class="bgimg" src="' + streetviewURL +'">');
