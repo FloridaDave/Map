7edffda11d0ca9e6946ae5e83b148475bdd49a92
FloridaDave <tohighdesertdave@yahoo.com>
2016-08-25 16:44:32 -0400

    commented out NYT code note

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address +'';
     $body.append('<img class="bgimg" src="' + streetviewURL +'">');
 
-    NT Times AJAX request
+    // NT Times AJAX request
 
     var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242'
     $.getJSON(nytimesURL, function(data){
