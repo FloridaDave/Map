c31c7a3f063b7db653c1c132e5d8bab8059addbe
FloridaDave <tohighdesertdave@yahoo.com>
2016-08-25 16:42:25 -0400

    2nd attempt on NYT code

--- js/script.js
+++ js/script.js
@@ -22,24 +22,24 @@ function loadData() {
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address +'';
     $body.append('<img class="bgimg" src="' + streetviewURL +'">');
 
-    // NT Times AJAX request
+    NT Times AJAX request
 
-    // var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242'
-    // $getJSON(nytimesURL, function(data){
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242'
+    $.getJSON(nytimesURL, function(data){
 
-    //     $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
-    //     articles = data.response.docs;
-    //     for (var i = 0; i < articles.length; i++) {
-    //         var article = articles[i];
-    //         $nytElem.append('li class="article">'+
-    //             '<a href="'+article.web_url+'">'+article.headline.main+
-    //             '</a>'+
-    //             '<p>' + article.snippet + '</p>'+
-    //         '</li>');
-    //     };
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+
+                '</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
 
-    // })
+    })
 
     return false;
 };
