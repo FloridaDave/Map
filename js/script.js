e3147ddcc940dfce64e34001b6023095a895fee7
FloridaDave <tohighdesertdave@yahoo.com>
2016-08-25 21:44:30 -0400

    commented out NYT

--- js/script.js
+++ js/script.js
@@ -24,22 +24,22 @@ function loadData() {
 
     // NY Times AJAX request
 
-    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242'
-    $.getJSON(nytimesURL, function(data){
+    // var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9ea1aa776bff425da7a60de26822c242'
+    // $.getJSON(nytimesURL, function(data){
 
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    //     $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+
-                '</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
+    //     articles = data.response.docs;
+    //     for (var i = 0; i < articles.length; i++) {
+    //         var article = articles[i];
+    //         $nytElem.append('li class="article">'+
+    //             '<a href="'+article.web_url+'">'+article.headline.main+
+    //             '</a>'+
+    //             '<p>' + article.snippet + '</p>'+
+    //         '</li>');
+    //     };
 
-    })
+    // })
 
     return false;
 };
